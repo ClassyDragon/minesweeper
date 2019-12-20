e1825f571279695f3678989263504c6d512a7823
ClassyDragon <cgandeg@gmail.com>
2019-12-19 20:56:28 -0500

    State enumerations

--- src/global.h
+++ src/global.h
@@ -1,11 +1,39 @@
 #pragma once
 
+// Screen pixel offset:
 const int verticalOffset = 50;
 const int horizontalOffset = 50;
 
+// Field dimensions:
 const int nWidth = 10;
 const int nHeight = 10;
+
+// Number of keys to poll:
+const int numKeys = 1;
 
 enum StateID {
     OVER = 0
 };
+
+// Key ID's:
+enum Keys {
+    LEFT_CLICK = 0
+};
+
+// State of keyboard key / mouse button:
+enum KeyState {
+    NOT_PRESSED = 0,
+    PRESSED = 1
+};
+
+// Type of tile:
+enum TileType {
+    MINE = 0,
+    VACANT = 1
+}
+
+// State of tile:
+enum TileState {
+    REVEALED = 0,
+    HIDDEN = 1
+}
